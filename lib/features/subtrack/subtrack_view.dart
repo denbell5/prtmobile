e9c5c15ed90aaf035f8135cca8c473da0362445c
denbell <denbell@ukr.net>
2021-10-24 13:14:13 +0300

    add basic subtrack_view layout

--- lib/features/subtrack/subtrack_view.dart
+++ lib/features/subtrack/subtrack_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:prtmobile/components/components.dart';
 import 'package:prtmobile/models/models.dart';
+import 'package:prtmobile/styles/styles.dart';
 
 class SubtrackView extends StatelessWidget {
   const SubtrackView({
@@ -9,12 +11,75 @@ class SubtrackView extends StatelessWidget {
 
   final Subtrack subtrack;
 
+  Widget _buildControlPanel(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        InlineButton(text: 'Delete', onTap: () {}),
+        const SizedBox(width: kHorizontalPadding),
+        InlineButton(text: 'Edit', onTap: () {}),
+      ],
+    );
+  }
+
+  Widget _buildRange(BuildContext context) {
+    final textStyle = AppTypography.h2;
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          subtrack.start.toString(),
+          style: textStyle,
+        ),
+        Text(
+          subtrack.pointer.toString(),
+          style: textStyle,
+        ),
+        Text(
+          subtrack.end.toString(),
+          style: textStyle,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      children: [
-        Text(subtrack.start.toString()),
-      ],
+    final scaleFactor = MediaQuery.of(context).textScaleFactor;
+    final rangeTextHeight = calcTextSize(
+      fontSize: FontSizes.h2,
+      scaleFactor: scaleFactor,
+    );
+    final formTextHeight = calcTextSize(
+      fontSize: FontSizes.body,
+      scaleFactor: scaleFactor,
+    );
+    const verticalPadding = kHorizontalPadding / 4;
+    final totalHeight =
+        rangeTextHeight + formTextHeight + (verticalPadding * 8);
+
+    return Padding(
+      padding: const EdgeInsets.symmetric(
+        horizontal: kHorizontalPadding,
+      ),
+      child: SizedBox(
+        height: totalHeight,
+        child: Stack(
+          children: [
+            Positioned(
+              top: verticalPadding,
+              child: _buildControlPanel(context),
+            ),
+            Positioned.fill(
+              child: _buildRange(context),
+            ),
+            const Positioned(
+              bottom: verticalPadding,
+              child: Text('Start must have a value.'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
