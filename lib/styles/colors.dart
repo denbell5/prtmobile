4afaef766ef11d98703459db4c0f4fff94a9364d
denbell <denbell@ukr.net>
2021-10-31 21:38:26 +0200

    make grey color darker

--- lib/styles/colors.dart
+++ lib/styles/colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/widgets.dart';
 
 class AppColors {
   static Color get white => Colors.grey[50]!;
-  static Color get grey => Colors.grey;
+  static Color get grey => Colors.grey[600]!;
   static Color get lightGrey => Colors.grey[200]!;
   static Color get black => Colors.black;
   static Color get transparent => Colors.transparent;
