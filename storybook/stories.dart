02c3f3df57084268fa56a4a6e81981b71e41414b
denbell5 <denbell712@gmail.com>
2022-01-23 23:17:06 +0200

    remove old input

--- storybook/stories.dart
+++ storybook/stories.dart
@@ -1,6 +1,5 @@
 import 'stories/bottom_dialog.stories.dart';
 import 'stories/db.stories.dart';
-import 'stories/editable_text.stories.dart';
 import 'stories/expandable.stories.dart';
 import 'stories/expandable_list_poc.stories.dart';
 import 'stories/picker_list.stories.dart';
@@ -9,6 +8,7 @@ import 'stories/text_size.stories.dart';
 import 'stories/theme.stories.dart';
 import 'stories/touchable_color.stories.dart';
 import 'stories/touchable_opacity.stories.dart';
+
 import 'storybook.dart';
 
 final stories = <StorybookStory>[
@@ -16,7 +16,6 @@ final stories = <StorybookStory>[
   BottomDialogStories(),
   SubtrackUpdateStories(),
   ListPickerStories(),
-  EditableTextStories(),
   TextSizeStories(),
   TouchableColorStories(),
   TouchableOpacityStories(),
