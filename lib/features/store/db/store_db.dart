3ff8a05afa4073279703401f0ff732b49b4e1652
denbell <denbell@ukr.net>
2022-03-19 20:50:23 +0200

    use firestore to get tracksets for students

--- lib/features/store/db/store_db.dart
+++ lib/features/store/db/store_db.dart
@@ -1,31 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:prtmobile/features/store/store.dart';
 
 class TrackingStoreDb {
+  late final FirebaseFirestore _db;
+
   Future<void> init() async {
-    await Future<void>.delayed(const Duration(seconds: 1));
+    await Firebase.initializeApp();
+    _db = FirebaseFirestore.instance;
   }
 
   Future<List<TracksetSo>> getTracksets() async {
-    await Future<void>.delayed(const Duration(seconds: 1));
-    final subtracks = <SubtrackSo>[
-      const SubtrackSo(start: 10, end: 20),
-      const SubtrackSo(start: 30, end: 40),
-    ];
-    final tracks = <TrackSo>[
-      TrackSo(name: 'Read Descrete math', subtracks: subtracks),
-      TrackSo(name: 'Read Physics: Mechanics', subtracks: subtracks),
-    ];
-    return [
-      TracksetSo(
-        name: 'CE 1-1',
-        recommendedDays: 90,
-        tracks: tracks,
-      ),
-      TracksetSo(
-        name: 'CE 1-2',
-        recommendedDays: 90,
-        tracks: tracks,
-      ),
-    ];
+    final snapshot = await _db.collection('tracksets').get();
+    final tracksets =
+        snapshot.docs.map((x) => TracksetSoMaps.fromRaw(x.data())).toList();
+    return tracksets;
   }
 }
--- lib/features/store/models/store.mappings.dart
+++ lib/features/store/models/store.mappings.dart
@@ -14,6 +14,13 @@ extension SubtrackSoMaps on SubtrackSo {
       pointer: start,
     );
   }
+
+  static SubtrackSo fromRaw(Map<String, dynamic> raw) {
+    return SubtrackSo(
+      start: raw['start'],
+      end: raw['end'],
+    );
+  }
 }
 
 extension TrackSoMaps on TrackSo {
@@ -28,6 +35,19 @@ extension TrackSoMaps on TrackSo {
       tracksetId: tracksetId,
       name: name,
       subtracks: normalizeSubtracks(subtracks),
+    );
+  }
+
+  static TrackSo fromRaw(Map<String, dynamic> raw) {
+    final subtracksRaw = raw['subtracks'] as List<dynamic>?;
+    final subtracks = subtracksRaw == null
+        ? <SubtrackSo>[]
+        : subtracksRaw
+            .map((x) => SubtrackSoMaps.fromRaw(x as Map<String, dynamic>))
+            .toList();
+    return TrackSo(
+      name: raw['name'],
+      subtracks: subtracks,
     );
   }
 }
@@ -45,6 +65,20 @@ extension TracksetSoMaps on TracksetSo {
       startAt: dateRange.start,
       endAt: dateRange.end,
       tracks: normalizeTracks(tracks),
+    );
+  }
+
+  static TracksetSo fromRaw(Map<String, dynamic> raw) {
+    final tracksRaw = raw['tracks'] as List<dynamic>?;
+    final tracks = tracksRaw == null
+        ? <TrackSo>[]
+        : tracksRaw
+            .map((x) => TrackSoMaps.fromRaw(x as Map<String, dynamic>))
+            .toList();
+    return TracksetSo(
+      name: raw['name'],
+      recommendedDays: raw['recommendedDays'],
+      tracks: tracks,
     );
   }
 }
