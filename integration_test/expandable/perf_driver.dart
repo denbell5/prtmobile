0517bde706e2dedd1fa52ae63069e57981201fa3
denbell <denbell@ukr.net>
2022-04-03 15:36:51 +0300

    add time label to test resuls

--- integration_test/expandable/perf_driver.dart
+++ integration_test/expandable/perf_driver.dart
@@ -1,7 +1,10 @@
 import 'package:integration_test/integration_test_driver.dart';
 import 'package:flutter_driver/flutter_driver.dart' as driver;
 
-String testResultLabel = '.default';
+String testResultLabel = () {
+  var now = DateTime.now().toUtc();
+  return '${now.month}-${now.day}-${now.hour}-${now.hour}-${now.minute}-${now.second}';
+}.call();
 
 Future<void> main() {
   return integrationDriver(
@@ -10,7 +13,7 @@ Future<void> main() {
         final timeline = driver.Timeline.fromJson(data['expandable']);
         final summary = driver.TimelineSummary.summarize(timeline);
         await summary.writeTimelineToFile(
-          'expandable$testResultLabel',
+          'expandable.$testResultLabel',
           destinationDirectory: 'integration_test/expandable/test_results/',
           pretty: true,
           includeSummary: true,
